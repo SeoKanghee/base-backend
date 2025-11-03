c0c2cddda2396abfe302a1f141d06b8336b0dca5
Kelly Seo <sylphnb@naver.com>
2025-11-03 12:57:14 +0900

    네이티브 라이브러리 로딩 방식에서 static 제거

--- src/main/java/com/kelly/base/common/jni/InternalLibLoader.java
+++ src/main/java/com/kelly/base/common/jni/InternalLibLoader.java
@@ -1,11 +1,16 @@
 package com.kelly.base.common.jni;
 
+import com.kelly.base.common.interfaces.IInternalLibLoader;
+import org.springframework.stereotype.Component;
+
 import java.io.InputStream;
 import java.nio.file.Paths;
 
-public interface InternalLibLoader {
-    static InputStream getLibInputStream(final String libNameExt) {
-        final String libPath = Paths.get("/", "native", libNameExt).toString();
-        return InternalLibLoader.class.getResourceAsStream(libPath);
+@Component
+public class InternalLibLoader implements IInternalLibLoader {
+    public InputStream getLibInputStream(final String libPath, final String libName) {
+        // jar 내 resource 에 있는 library 를 stream 으로 읽기 위한 path 정의
+        final String resourcePath = Paths.get("/", libPath, libName).toString();
+        return InternalLibLoader.class.getResourceAsStream(resourcePath);
     }
 }
